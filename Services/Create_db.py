1957f461b5c4924468ed8d43909d87dd6aa272b5
FengHaiF <haifeng_f@hust.edu.cn>
2019-12-21 20:59:55 +0800

    add sqlite3 as db,add delete function as update sql table send params for function service!!

--- Services/Create_db.py
+++ Services/Create_db.py
@@ -1,19 +1,27 @@
 import sqlite3
 import pandas as pd
 
-db_path = 'db/test_records.db'
+# db_path = 'db/test_records.db'
+db_path = 'db/test.db'
 
 def create_db(db_path):
     conn = sqlite3.connect(db_path)
     sqlQuery = conn.cursor()
-    sqlQuery.execute("CREATE TABLE RECORDS \
-              (ID INTEGER PRIMARY KEY AUTOINCREMENT,\
-               num real NOT NULL,\
-               x2 real NOT NULL,\
-               x4 real NOT NULL,\
-               temp real NOT NULL,\
-                y1 real, y2 real) ")
+    # sqlQuery.execute("CREATE TABLE RECORDS \
+    #           (ID INTEGER PRIMARY KEY AUTOINCREMENT,\
+    #            num real NOT NULL,\
+    #            x2 real NOT NULL,\
+    #            x4 real NOT NULL,\
+    #            temp real NOT NULL,\
+    #             y1 real, y2 real,) ")
 
+    sqlQuery.execute("CREATE TABLE RECORDS \
+                  (\
+                   num real NOT NULL,\
+                   x2 real NOT NULL,\
+                   x4 real NOT NULL,\
+                   temp real NOT NULL,\
+                    y1 real, y2 real) ")
     print("Create {0} db finish".format(db_path))
 
     conn.commit()
@@ -43,14 +51,33 @@ def save_record(db_path):
 def View_db(db_path):
     conn = sqlite3.connect(db_path)
     c = conn.cursor()
-    cursor = c.execute("SELECT id, num, x2, x4,temp,y1,y2  \
+    cursor = c.execute("SELECT  num, x2, x4,temp,y1,y2  \
                                 from records")
     for row in cursor:
-        print("ID = ", row[0])
-        print("x = {},{},{},{}".format(row[1],row[2],row[3],row[4]))
-        print("y1 ={0}, y2 = {1} ".format(row[5],row[6]), "\n")
+        # print("ID = ", row[0])
+        print("x = {},{},{},{}".format(row[0],row[1],row[2],row[3]))
+        print("y1 ={0}, y2 = {1} ".format(row[4],row[5]), "\n")
+    conn.close()
+
+
+
+def delete_db(db_path):
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+    delete_len = 20
+    # cursor.execute("DELETE FROM RECORDS WHERE id < "+ str(delete_len) )
+    # 删除前 delete_len 记录
+    tab_len = cursor.execute("SELECT count(*) from RECORDS")
+    print(list(tab_len)[0], "  tab_len")
+    # cursor.execute("DELETE FROM RECORDS WHERE id in (\
+    #                 SELECT id from RECORDS order by id asc\
+    #                  limit "+str(delete_len) +")")
+    # cursor.execute("VACUUM  "+ db_path)# id 重新开始更新
+    conn.commit()
+    conn.close()
 
 if __name__ == "__main__":
-    #create_db(db_path)
+    # create_db(db_path)
     # save_record(db_path)
+    delete_db(db_path)
     View_db(db_path)
